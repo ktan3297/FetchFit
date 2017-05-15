6c55caa07a388a1e816f4d847bd424e23d5532be
APPLE <apple@apples-mbp.fios-router.home>
2017-05-14 22:31:03 -0400

    ugly popup window

--- Fetch/ResultViewController.swift
+++ Fetch/ResultViewController.swift
@@ -21,6 +21,14 @@ class ResultViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func showPopup(_ sender: AnyObject) {
+        let popOverVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "sbPopUpID") as! PopUpViewController
+        self.addChildViewController(popOverVC)
+        popOverVC.view.frame = self.view.frame
+        self.view.addSubview(popOverVC.view)
+        popOverVC.didMove( toParentViewController: self)
+        
+    }
 
 }
 
