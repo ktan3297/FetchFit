51ae0447c13e2dc48e24989ece0519efa9a69d3c
Kristy Tan <Kristy@Kristys-MacBook-Pro.local>
2017-05-26 13:16:17 -0400

    added camera and fixed hidden keyboard

---
+++ Fetch/CameraAccessViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CameraAccessViewController.swift
+//  Fetch
+//
+//  Created by Kristy Tan on 5/26/17.
+//  Copyright © 2017 Kristy Tan. All rights reserved.
+//
+
+import UIKit
+
+class CameraAccessViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var imageDisplay: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func takePhoto(_ sender: UIButton) {
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)){
+            let cameraUsed = UIImagePickerController()
+            cameraUsed.delegate = self
+            cameraUsed.sourceType = UIImagePickerControllerSourceType.camera
+            self.present(cameraUsed, animated: true , completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageDisplay.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+        imageDisplay.contentMode = .scaleToFill
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
