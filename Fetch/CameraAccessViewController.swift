5626167ae853eda5cf3b74784465bcf0c54c0481
Kristy Tan <Kristy@Kristys-MacBook-Pro.local>
2017-06-01 12:29:43 -0400

    afdsfjd

--- Fetch/CameraAccessViewController.swift
+++ Fetch/CameraAccessViewController.swift
@@ -16,6 +16,8 @@ class CameraAccessViewController: UIViewController, UINavigationControllerDelega
     @IBOutlet weak var camSticker3: UIImageView!
     
     var heart1 = 0
+    var m = 0
+    var crown1 = 0
     
     @IBAction func tapSticker(_ sender: UIButton) {
         heart1 += 1
@@ -29,16 +31,32 @@ class CameraAccessViewController: UIViewController, UINavigationControllerDelega
     }
     
     @IBAction func tapSticker2(_ sender: UIButton) {
-        camSticker2.image = #imageLiteral(resourceName: "moustache")
+        m += 1
+        if(m == 1){
+            camSticker2.image = #imageLiteral(resourceName: "moustache")
+        }
+        else{
+            m = 0
+            camSticker2.image = nil
+        }
     }
 
     @IBAction func tapSticker3(_ sender: UIButton) {
-        camSticker3.image = #imageLiteral(resourceName: "pink_crown")
+        crown1 += 1
+        if(crown1 == 1){
+            camSticker3.image = #imageLiteral(resourceName: "pink_crown")
+        }
+        else{
+            crown1 = 0
+            camSticker3.image = nil
+        }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -59,6 +77,12 @@ class CameraAccessViewController: UIViewController, UINavigationControllerDelega
         imageDisplay.image = info[UIImagePickerControllerOriginalImage] as? UIImage
         dismiss(animated: true, completion: nil)
         imageDisplay.contentMode = .scaleToFill
+        if(imageDisplay.image != nil){
+            print("not nil")
+            camSticker1.isHidden = false
+            camSticker2.isHidden = false
+            camSticker3.isHidden = false
+        }
     }
 
     /*
